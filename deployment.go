67453d11a77194a027bca9c1d3cd7a16adf0351f
chrnorm <17420369+chrnorm@users.noreply.github.com>
2022-11-01 17:38:03 +0000

    improve logging

--- deployment.go
+++ deployment.go
@@ -1,7 +1,7 @@
 package analytics
 
 import (
-	"context"
+	"os"
 	"sync"
 
 	"github.com/common-fate/analytics-go/acore"
@@ -11,67 +11,6 @@ import (
 var (
 	depMutex     = &sync.RWMutex{}
 	globalDeploy *Deployment
-)
-
-type globalDep struct {
-	mu     *sync.Mutex
-	dep    *Deployment
-	loader DeploymentLoader
-}
-
-type DeploymentLoader interface {
-	LoadDeployment(ctx context.Context) (*Deployment, error)
-}
-
-func (gd *globalDep) SetLoader(loader DeploymentLoader) {
-	gd.loader = loader
-}
-
-func (gd *globalDep) Get(ctx context.Context) *Deployment {
-	// if we don't have a deployment and there is no loader defined,
-	// we can't load one. So just return with nil
-	if gd.dep == nil && gd.loader == nil {
-		return nil
-	}
-
-	// if we don't have a deployment, but we have a loader defined to fetch one,
-	// try and fetch it.
-	if gd.dep == nil {
-		gd.loadDeployment(ctx)
-	}
-
-	// if the deployment ID is empty also return nil.
-	if gd.dep.ID == "" {
-		return nil
-	}
-
-	// if we get here, we've got a good deployment.
-	return gd.dep
-}
-
-func (gd *globalDep) loadDeployment(ctx context.Context) {
-	if gd.loader == nil {
-		return
-	}
-
-	log := zap.L().Named("cf-analytics")
-	d, err := gd.loader.LoadDeployment(ctx)
-	if err != nil {
-		log.Error("error loading deployment", zap.Error(err))
-		// just set an empty deployment to prevent trying to load it again the next time.
-		d = &Deployment{}
-	}
-
-	gd.mu.Lock()
-	defer gd.mu.Unlock()
-
-	gd.dep = d
-}
-
-var (
-	globalDeployment = &globalDep{
-		mu: &sync.Mutex{},
-	}
 )
 
 // Deployment is a Common Fate deployment identifier.
@@ -106,4 +45,8 @@ func SetDeployment(dep *Deployment) {
 	depMutex.Lock()
 	defer depMutex.Unlock()
 	globalDeploy = dep
+
+	if os.Getenv("CF_ANALYTICS_DEBUG") == "true" {
+		zap.L().Named("cf-analytics").Info("set deployment", zap.Any("deployment", dep))
+	}
 }
