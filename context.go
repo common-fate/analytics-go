0f47ab9c300b338da8794060d8d933c1535948f4
chrnorm <17420369+chrnorm@users.noreply.github.com>
2022-11-01 18:28:45 +0000

    add context methods

---
+++ context.go
@@ -0,0 +1,29 @@
+package analytics
+
+import (
+	"context"
+
+	"github.com/common-fate/analytics-go/acore"
+)
+
+type contextKey struct {
+	name string
+}
+
+var analyticsClientContext = contextKey{name: "analyticsClientContext"}
+
+// FromContext loads an analytics client from context.
+// analytics.SetContext must have been called.
+// if there is no client in context, a new noop client is returned.
+func FromContext(ctx context.Context) *Client {
+	c, ok := ctx.Value(analyticsClientContext).(*Client)
+	if !ok {
+		return newClient(&acore.NoopClient{})
+	}
+	return c
+}
+
+// SetContext sets the analytics in context
+func SetContext(ctx context.Context, client *Client) context.Context {
+	return context.WithValue(ctx, analyticsClientContext, client)
+}
